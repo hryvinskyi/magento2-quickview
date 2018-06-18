e766e118a676b005227db97e3e9131d626c4ed0a
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2018-06-18 20:57:51 +0300

    Fixe configrable product

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,7 @@
 var config = {
     map: {
         '*': {
+            'Magento_Swatches/js/swatch-renderer': 'Script_QuickView/js/swatch-renderer',
             colorBox: 'Script_QuickView/js/jquery.colorbox-min.min',
             script_quickview: 'Script_QuickView/js/script-quickview'
         }
